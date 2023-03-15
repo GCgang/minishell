878eb17fe4fcf15290ac2746bf56468d9f3aaec9
Hyeon Jun An <hyeoan@c4r5s1.42seoul.kr>
2023-03-15 21:59:11 +0900

    [Rename]: builtin/cd

--- builtin/cd/cd.h
+++ builtin/cd/cd.h
@@ -6,7 +6,15 @@
 /*   By: hyeoan <hyeoan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:46:08 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/03/14 16:46:09 by hyeoan           ###   ########.fr       */
+/*   Updated: 2023/03/15 20:47:24 by hyeoan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#	ifndef CD_H
+# define CD_H
+
+# include "../../excute.h"
+
+void	built_in_cd(void);
+
+#endif
