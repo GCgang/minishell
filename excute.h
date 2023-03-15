5443657e21fc24c19535e7153b31bf5e4df639be
Hyeon Jun An <hyeoan@c4r5s1.42seoul.kr>
2023-03-15 21:56:17 +0900

    [Include]: excute.h

---
+++ excute.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   excute.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeoan <hyeoan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 16:00:55 by hyeoan            #+#    #+#             */
+/*   Updated: 2023/03/15 21:38:12 by hyeoan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// gcc excute.c strncmp.c ../builtin/cd/cd.c ../builtin/echo/echo.c ../builtin/env/env.c ../builtin/exit/exit.c ../builtin/export/export.c ../builtin/pwd/pwd.c ../builtin/unset/unset.c
+#	ifndef EXCUTE_H
+# define EXCUTE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "builtin.h"
+
+void	excute_simple_built_in(char **argv);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
