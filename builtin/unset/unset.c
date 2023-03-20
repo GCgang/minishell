4a382c1464d60812e31d588b3370c5cfc7e62a9e
HJ <powm97@gmail.com>
2023-03-21 01:48:14 +0900

    builtin ing

--- builtin/unset/unset.c
+++ builtin/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: jun <jun@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:27:05 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/03/20 11:25:37 by jun              ###   ########.fr       */
+/*   Updated: 2023/03/21 01:46:54 by jun              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,37 @@ int	check_unset_syntax(char *variable)
 	return (1);
 }
 
-void	built_in_unset(char **envp)
+void	delete_envp(t_env *env_list, char *name)
+{
+	t_env	*tmp_list;
+
+	tmp_list = env_list->next;
+	while (tmp_list != NULL)
+	{
+		if (ft_strcmp(tmp_list->name, name) == 0)
+		{
+			//del list
+			return ;
+		}
+		tmp_list = tmp_list->next;
+	}
+}
+
+void	built_in_unset(t_command **cmd, t_env *env_list)
 {
 	int	i;
 
-	i = -1;
-	while (envp[++i])
+	i = 0;
+	while ((*cmd)->word[++i])
 	{
-		if (check_unset_syntax(envp[i]) == 0)
+		if (check_unset_syntax((*cmd)->word[i]) == 0)
 		{
 			ft_putstr_fd("not a valid identifier", 2);
-			//g_exit_status = 1;
+			env_list->status = 1;
 		}
 		else
 		;	
-			//delete_envp(envp);
+			//delete_envp(env_list, (*cmd)->word[i]);
 	}
 	return ;
 }
--- builtin/unset/unset.h
+++ builtin/unset/unset.h
@@ -6,7 +6,7 @@
 /*   By: jun <jun@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:46:26 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/03/20 11:25:27 by jun              ###   ########.fr       */
+/*   Updated: 2023/03/21 01:37:45 by jun              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 # include "../../execute.h"
 
-void	built_in_unset(char **envp);
+void	built_in_unset(t_command **cmd, t_env *env_list);
 #endif
