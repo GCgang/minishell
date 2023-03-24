8b95a3b218f9a40e412aecaec77904e08fb189cd
Hyeon Jun An <hyeoan@c10r4s4.42seoul.kr>
2023-03-24 13:11:48 +0900

    test: builtin

--- exec/builtin/echo/echo.c
+++ exec/builtin/echo/echo.c
@@ -6,48 +6,29 @@
 /*   By: hyeoan <hyeoan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:26:52 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/03/23 20:00:32 by hyeoan           ###   ########.fr       */
+/*   Updated: 2023/03/24 12:54:39 by hyeoan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "echo.h"
 
-// -nnnn -nnnn
-// void	check_echo_option(char **cmd, int *option_n, int *idx)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (cmd[1][i] != '-')
-// 		return ;
-// 	while (cmd[1][++i] == 'n')
-// 		(*option_n)++;
-// 	if (cmd[1][i] != '\0')
-// 	{
-// 		*option_n = 0;
-// 		return ;
-// 	}
-// 	*option_n = 1;
-// 	(*idx)++;
-// }
-
-void	check_echo_option(char *cmd, int *option_n, int *idx)
+int	check_echo_option(char *cmd, int *option_n)
 {
 	int	i;
 
 	i = 0;
 	if (cmd[i] != '-')
-		return ;
-	while (cmd[++i] == 'n')
-		(*option_n)++;
-	if (cmd[i] != '\0')
 	{
-		*option_n = 0;
-		return ;
+		return (0);
 	}
-	*option_n = 1;
-	(void)idx;
-	//(*idx)++;
+	while (cmd[++i] == 'n')
+		;
+	if (cmd[i] == '\0')
+	{
+		*option_n = 1;
+		return (1);
+	}
+	return (0);
 }
 
 void	built_in_echo(t_command **cmd, t_env *env_list)
@@ -63,11 +44,15 @@ void	built_in_echo(t_command **cmd, t_env *env_list)
 	{
 		while ((*cmd)->word[++idx] != NULL)
 		{
-			check_echo_option((*cmd)->word[idx], &option_n, &idx);
-			if (option_n == 0)
-				ft_putstr_fd((*cmd)->word[idx], 1);
+			if (check_echo_option((*cmd)->word[idx], &option_n) == 0)
+				break ;
+		}
+		while ((*cmd)->word[idx] != NULL)
+		{
+			ft_putstr_fd((*cmd)->word[idx], 1);
 			if ((*cmd)->word[idx + 1] != NULL)
 				ft_putchar_fd(' ', 1);
+			idx++;
 		}
 		if (option_n == 0)
 			ft_putchar_fd('\n', 1);
