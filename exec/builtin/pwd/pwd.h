a1c6f815b52db3ec30e40a73e0a8058d684d5384
Hyeon Jun An <hyeoan@c4r5s1.42seoul.kr>
2023-03-26 21:00:42 +0900

    feat(pwd): Prints the absolute pathname of the current working directory.
    The return status is 0 unless an error occurs or an incorrect option is provided while resolving the current directory name.

--- exec/builtin/pwd/pwd.h
+++ exec/builtin/pwd/pwd.h
@@ -6,13 +6,13 @@
 /*   By: hyeoan <hyeoan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 10:44:52 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/03/20 16:15:59 by hyeoan           ###   ########.fr       */
+/*   Updated: 2023/03/26 19:54:00 by hyeoan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef PWD_H
 # define PWD_H
-# define PATH_MAX 1024 // limit.h -> PATH_MAX 1024 (del)
+# define PATH_MAX 1024
 # include "../../include/execute.h"
 
 void	built_in_pwd(t_env *env_list);
