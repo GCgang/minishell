e99c089576e43266fc0bf08d71ec4f45b49afd58
GCgang <powm97@gmail.com>
2023-04-11 13:34:23 +0900

    fix(here_doc)

--- exec/include/here_doc.h
+++ exec/include/here_doc.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeoan <hyeoan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jun <jun@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:14:12 by hyeoan            #+#    #+#             */
-/*   Updated: 2023/04/07 16:50:31 by hyeoan           ###   ########.fr       */
+/*   Updated: 2023/04/11 00:51:36 by jun              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,6 @@
 # define HERE_DOC_H
 
 # include "execute.h"
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
-
-typedef struct s_node
-{
-	int				fd;
-	char			*back_up;
-	struct s_node	*next;
-}	t_file_node;
-
-typedef struct s_list
-{
-	t_file_node	*tail;
-	t_file_node	*before;
-	t_file_node	*current;
-	size_t		total_fd;
-}	t_list;
 
 int		check_here_document(t_command *process);
 int		here_document(t_command *process, int idx);
